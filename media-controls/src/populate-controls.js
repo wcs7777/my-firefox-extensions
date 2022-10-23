ffa1adbabbbe78449d88a4672b14bd5090e32ecf
Willian Carlos <wcs777git@outlook.com>
2022-10-23 17:32:37 -0300

    Media Controls: remove PageUp and PageDown controls

--- media-controls/src/populate-controls.js
+++ media-controls/src/populate-controls.js
@@ -1,7 +1,7 @@
 export default function populateControls(table) {
 	return table.set({
-		begin: ["0", "F2", "Home", "PageUp"],
-		end: ["9", "End", "PageDown"],
+		begin: ["0", "F2", "Home"],
+		end: ["9", "End"],
 		middle: ["1", "2", "3", "4", "5", "6", "7", "8"],
 		backward: ["ArrowLeft", "MediaTrackPrevious"],
 		forward: ["ArrowRight", "MediaTrackNext"],
