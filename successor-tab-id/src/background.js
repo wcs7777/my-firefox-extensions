31ee17f27ae1d23f8d82afa60addcb12c8a3341e
Willian Carlos <wcs777git@outlook.com>
2022-10-26 03:05:46 -0300

    Sucessor Tab Id: version 1.0.0

---
+++ successor-tab-id/src/background.js
@@ -0,0 +1,99 @@
+import populateOptions from "./populate-options.js";
+import { utilsTable, optionsTable } from "./tables.js";
+
+(async () => {
+	if (!await utilsTable.get(optionsTable.name)) {
+		await populateOptions(optionsTable);
+		await utilsTable.set(optionsTable.name, true);
+	}
+	if (!browser.browserAction.onClicked.hasListener(actionOnClicked)) {
+		browser.browserAction.onClicked.addListener(actionOnClicked);
+	}
+	if (!browser.storage.onChanged.hasListener(storageOnChanged)) {
+		browser.storage.onChanged.addListener(storageOnChanged);
+	}
+	await updateActivated();
+	return "Initialization finished";
+})()
+	.then(console.log)
+	.catch(console.error);
+
+async function actionOnClicked() {
+	try {
+		await optionsTable.set(
+			"activated",
+			!await optionsTable.get("activated"),
+		);
+		await updateActivated();
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function updateActivated() {
+	if (await optionsTable.get("activated")) {
+		console.log("activated");
+		if (!browser.tabs.onActivated.hasListener(onActivated)) {
+			browser.tabs.onActivated.addListener(onActivated);
+		}
+		await browser.browserAction.setIcon({
+			path: {
+				16: "../icons/icon-16.png",
+				32: "../icons/icon-32.png",
+			},
+		});
+	} else {
+		console.log("deactivated");
+		if (browser.tabs.onActivated.hasListener(onActivated)) {
+			browser.tabs.onActivated.removeListener(onActivated);
+		}
+		await setAllSuccessorToNone();
+		await browser.browserAction.setIcon({
+			path: {
+				16: "../icons/icon-dark-16.png",
+				32: "../icons/icon-dark-32.png",
+			},
+		});
+	}
+}
+
+async function storageOnChanged(changes) {
+	try {
+		if (changes[optionsTable.name]) {
+			await updateActivated();
+		}
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function onActivated({ tabId, previousTabId }) {
+	try {
+		if (previousTabId) {
+			console.log("before", await browser.tabs.get(tabId));
+			await setSuccessor(tabId, previousTabId);
+			console.log("after", await browser.tabs.get(tabId));
+		} else {
+			console.log("without previousTabId");
+		}
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+function setSuccessor(tabId, successorId) {
+	return browser.tabs.moveInSuccession(
+		[tabId],
+		successorId,
+		{
+			append: false,
+			insert: true,
+		},
+	);
+}
+
+async function setAllSuccessorToNone() {
+	for (const { id } of await browser.tabs.query({ currentWindow: true })) {
+		await setSuccessor(id, browser.tabs.TAB_ID_NONE);
+	}
+}
---
+++ successor-tab-id/src/tables.js
@@ -0,0 +1,6 @@
+import Table from "./table.js";
+import localStorage from "./local-storage.js";
+
+export const database = localStorage;
+export const optionsTable = new Table("options", database);
+export const utilsTable = new Table("utils", database);
