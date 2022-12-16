8d95df4970b127b3c6fdc586ff783a44bdea9697
Willian Carlos <wcs777git@outlook.com>
2022-12-16 16:00:34 -0300

    media controls: add browser action

--- media-controls/src/background.js
+++ media-controls/src/background.js
@@ -3,12 +3,16 @@ import populateOptions from "./populate-options.js";
 import { utilsTable, optionsTable, controlsTable } from "./tables.js";
 
 (async () => {
-	populate(optionsTable, populateOptions)
-		.then(console.log)
-		.catch(console.error);
 	populate(controlsTable, populateControls)
 		.then(console.log)
 		.catch(console.error);
+	await populate(optionsTable, populateOptions)
+		.then(console.log)
+		.catch(console.error);
+	if (!browser.browserAction.onClicked.hasListener(actionOnClicked)) {
+		browser.browserAction.onClicked.addListener(actionOnClicked);
+	}
+	await updateActivated(await optionsTable.get("activated"));
 	return "Initialization finished";
 })()
 	.then(console.log)
@@ -20,4 +24,34 @@ async function populate(table, fn) {
 		await utilsTable.set(table.name, true);
 	}
 	return `${table.name} populated`;
+}
+
+async function actionOnClicked() {
+	try {
+		const toggle = !await optionsTable.get("activated");
+		await optionsTable.set("activated", toggle);
+		await updateActivated(toggle);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function updateActivated(activated) {
+	if (activated) {
+		console.log("activated");
+		await browser.browserAction.setIcon({
+			path: {
+				16: "../icons/icon-16.png",
+				32: "../icons/icon-32.png",
+			},
+		});
+	} else {
+		console.log("deactivated");
+		await browser.browserAction.setIcon({
+			path: {
+				16: "../icons/icon-dark-16.png",
+				32: "../icons/icon-dark-32.png",
+			},
+		});
+	}
 }
